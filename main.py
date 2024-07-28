6726db5689dafd8cfc732de97c43603c50010dd2
RAIT-09 <rainbowtyourou.0963@gmail.com>
2024-07-28 17:57:16 +0900

    change to use tempfile

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from zip import Zip
 import os
+import tempfile
 from merger import Merger
 
 old_dump = "./slackdump_20240401_20240713.zip"
 new_dump = "./slackdump_20240401_20240725.zip"
-Zip.extract(old_dump, "./.tmp/old")
-Zip.extract(new_dump, "./.tmp/new")
 
-if not os.path.exists("./.tmp/merged"):
-    os.makedirs("./.tmp/merged")
-    Merger("./.tmp/old/", "./.tmp/new/", "./.tmp/merged/")
+with tempfile.TemporaryDirectory() as temp_dir:
+    Zip.extract(old_dump, f"{temp_dir}/old")
+    Zip.extract(new_dump, f"{temp_dir}/new")
 
-Zip.compress("./.tmp/merged/", "merged.zip")
+    if not os.path.exists(f"{temp_dir}/merged"):
+        os.makedirs(f"{temp_dir}/merged")
+        Merger(f"{temp_dir}/old/", f"{temp_dir}/new/", f"{temp_dir}/merged/")
+
+    Zip.compress(f"{temp_dir}/merged/", "merged.zip")
